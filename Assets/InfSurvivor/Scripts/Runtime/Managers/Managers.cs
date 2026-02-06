d55abce2c5f55bb4885b41f618a7ed245e9d6426
YiDoSeon <lds3794@gmail.com>
2026-02-06 16:46:27 +0900

    feat(manager): Managers 생성 로직 수정

--- Assets/InfSurvivor/Scripts/Runtime/Managers/Managers.cs
+++ Assets/InfSurvivor/Scripts/Runtime/Managers/Managers.cs
@@ -5,13 +5,22 @@ namespace InfSurvivor.Runtime.Manager
     public class Managers : MonoBehaviour
     {
         #region Singleton
+        private const string MANGER_NAME = "@Managers";
         public static bool Initialized { get; private set; }
         private static Managers instance;
         public static Managers Instance
         {
             get
             {
-                Init();
+                if (instance == null)
+                {
+                    Managers manager = FindFirstObjectByType<Managers>();
+                    if (manager == null)
+                    {
+                        GameObject go = new GameObject();
+                        go.AddComponent<Managers>();                        
+                    }
+                }
                 return instance;
             }
         }
@@ -22,30 +31,32 @@ namespace InfSurvivor.Runtime.Manager
         public static CollisionManager Collision => Instance.collision;
         #endregion
 
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                gameObject.name = MANGER_NAME;
+
+                DontDestroyOnLoad(gameObject);
+                Init();
+            }
+            else if(instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
         private static void Init()
         {
-            if (instance == null && Initialized == false)
-            {
-                Initialized = true;
-
-                GameObject go = GameObject.Find("@Managers");
-                if (go == null)
-                {
-                    go = new GameObject("@Managers");
-                    go.AddComponent<Managers>();
-                }
-
-                DontDestroyOnLoad(go);
-
-                instance = go.GetComponent<Managers>();
-                Create(ref instance.collision, nameof(instance.collision));
-            }
+            Debug.Assert(Initialized == false, "Managers가 중복 초기화되었습니다.");
+            Create(ref instance.collision, nameof(instance.collision));
         }
 
         private static void Create<T>(ref T target, string name) where T : BehaviourManagerBase
         {
             Debug.Assert(target == null, "이미 생성되었습니다.");
-            GameObject go = new GameObject(name);
+            GameObject go = new GameObject($"@{name}");
             go.transform.parent = instance.transform;
             target = go.AddComponent<T>();
             target.Init();
